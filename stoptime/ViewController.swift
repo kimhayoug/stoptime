190749710b3d2aa25bd01dcd8c84830cdf4dd63a
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-04-09 16:33:59 +0900

    stop

--- stoptime/ViewController.swift
+++ stoptime/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+     var count = 0
+     var mytimer = Timer()
+     var time  = 0
+    @IBOutlet weak var timelabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    func updateTime(){
+        count = count + 1
+        let min = count / 60 / 100
+        let sec = (count - (min * 60 * 100)) / 100
+        let msec = count - (min * 60 * 100) - (sec * 100)
+        timelabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
+    }
+    
+    @IBAction func start(_ sender: Any) {
+        mytimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block:{ (mytimer) in self.updateTime()})
+    }
+    @IBAction func stop(_ sender: Any) {
+        mytimer.invalidate()
+    }
+    
+    @IBAction func zreo(_ sender: Any) {
+       count = 0
+        timelabel.text = "00:00:00"
+    
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
